1c001c380550d42df3177edb35b549f0b78f55d7
izay komrink (sukma wijaya) <izay_tea@yahoo.com>
2012-11-16 10:01:05 +0700

    sesi-05 & Event selesai

---
+++ Tugas Praktikum/sesi-05/belajar-event/src/main/java/belajar/event/BlankArea.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BlankArea extends JLabel{
+	Dimension miniSize = new Dimension(100,50);
+	
+	public BlankArea(Color color){
+		setBackground(color);
+		setOpaque(true);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+	}
+	
+	public Dimension getMinimumSize(){
+		return miniSize;
+	}
+	
+	public Dimension getPreferredSize(){
+		return miniSize;
+	}
+}
